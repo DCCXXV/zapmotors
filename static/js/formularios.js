f04f82cf653eb70d4f95e3ede42b01a52b79a9cb
hwenj <146937278+hwenj@users.noreply.github.com>
2025-10-18 17:57:05 +0200

    Barra de progreso
    Co-Authored-By: Marco Antonio <110636189+DCCXXV@users.noreply.github.com>

--- static/js/formularios.js
+++ static/js/formularios.js
@@ -4,6 +4,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const dateInput = document.querySelector("#dateInput");
     const durationInput = document.querySelector("#durationInput");
     const submitBtn = document.querySelector("#submitBtn");
+    const vehicle = document.querySelector("#selectedVehicle");
+    const startTime = document.querySelector("#startTime");
+    const endtTime = document.querySelector("#endTime");
+    const checkboxInput = document.querySelector("#checkboxInput");
+    const phone = document.querySelector("#phone");
 
     const fullNameInputError = document.querySelector("#fullNameError");
     const emailInputError = document.querySelector("#emailError");
@@ -11,12 +16,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const durationInputError = document.querySelector("#durationInputError");
 
     const progressBar = document.querySelector("#progressBar")
-    let progress = 0;
-    let increment = 100/9;
+    let progress = Array(9).fill(false);
+    let increment = 100 / 9;
 
     function updateProgressBar() {
-        progressBar.style.width = `${progress}%`;
-        progressBar.setAttribute("aria-valuenow", progress);
+        let total = 0;
+        for (let i = 0; i < progress.length; i++) {
+            if (progress[i]) {
+                total += 100 / 9;
+            }
+        }
+        progressBar.style.width = `${total}%`;
+        progressBar.setAttribute("aria-valuenow", total);
     }
 
     fullNameInput.addEventListener("input", function () {
@@ -26,14 +37,14 @@ document.addEventListener("DOMContentLoaded", function () {
             fullNameInputError.innerHTML = "El nombre y apellidos deben tener al menos 3 caracteres.";
             fullNameInputError.style.display = "block";
             fullNameInput.style.borderColor = "red";
-            return;
+            progress[0] = false;
         } else {
             fullNameInput.style.borderColor = "green";
             fullNameInputError.style.display = "none";
-            progress += increment;
-            updateProgressBar();
             fullNameInput.dataset.valid = "false";
+            progress[0] = true;
         }
+        updateProgressBar();
     });
 
     emailInput.addEventListener("input", function () {
@@ -43,11 +54,13 @@ document.addEventListener("DOMContentLoaded", function () {
             emailInputError.innerHTML = "Por favor, introduce un correo electrónico válido.";
             emailInputError.style.display = "block";
             emailInput.style.borderColor = "red";
-            return;
+            progress[1] = false;
         } else {
             emailInput.style.borderColor = "green";
             emailInputError.style.display = "none";
+            progress[1] = true;
         }
+        updateProgressBar();
     });
 
     dateInput.addEventListener("input", function () {
@@ -59,21 +72,70 @@ document.addEventListener("DOMContentLoaded", function () {
             dateInputError.innerHTML = "Por favor, escoja una fecha posterior al actual.";
             dateInputError.style.display = "block";
             dateInput.style.borderColor = "red";
-        } else{
+            progress[2] = false;
+        } else {
             dateInput.style.borderColor = "green";
             dateInputError.style.display = "none";
+            progress[2] = true;
         }
+        updateProgressBar();
     });
 
-    durationInput.addEventListener("input", function() {
-        if (durationInput.value < 0) {
+    durationInput.addEventListener("input", function () {
+        if (durationInput.value < 0 || durationInput.value == "") {
             durationInputError.innerHTML = "Por favor, introduzca una duración postiva.";
             durationInput.style.borderColor = "red";
             durationInputError.style.display = "block"
+            progress[3] = false;
         } else {
             durationInput.style.borderColor = "green";
+            progress[3] = true;
         }
+        updateProgressBar();
     })
+
+    checkboxInput.addEventListener("click", function () {
+        if (checkboxInput.checked) {
+            progress[4] = true;
+        } else {
+            progress[4] = false;
+        }
+        updateProgressBar();
+    });
+
+    vehicle.addEventListener("click", function () {
+        if (vehicle.value !== "") {
+            progress[5] = true;
+        }
+        updateProgressBar();
+    });
+
+    startTime.addEventListener("input", function () {
+        if (startTime.value !== "") {
+            progress[6] = true;
+        } else {
+            progress[6] = false;
+        }
+        updateProgressBar();
+    });
+
+    endTime.addEventListener("input", function () {
+        if (endTime.value !== "") {
+            progress[7] = true;
+        } else {
+            progress[7] = false;
+        }
+        updateProgressBar();
+    });
+
+    phone.addEventListener("input", function () {
+        if (phone.value.length < 9) {
+            progress[8] = false;
+        } else {
+            progress[8] = true;
+        }
+        updateProgressBar();
+    });
 
     submitBtn.addEventListener("click", function (event) {
         event.preventDefault();
@@ -119,7 +181,7 @@ document.addEventListener("DOMContentLoaded", function () {
         alert("Formulario enviado correctamente");
     });
 
-    resetBtn.addEventListener("click", function() {
+    resetBtn.addEventListener("click", function () {
         fullNameInputError.style.display = "none";
         emailInputError.style.display = "none";
         dateInputError.style.display = "none";
